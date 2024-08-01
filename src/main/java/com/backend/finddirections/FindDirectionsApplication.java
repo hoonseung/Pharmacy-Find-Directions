da5dd318ca90d4ab1b25666a8884f5b47647c792
lsh <tmdgns4029@naver.com>
2024-08-01 23:02:11 +0900

    first commit

---
+++ src/main/java/com/backend/finddirections/FindDirectionsApplication.java
@@ -0,0 +1,13 @@
+package com.backend.finddirections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindDirectionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindDirectionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/finddirections/FindDirectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.finddirections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindDirectionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
