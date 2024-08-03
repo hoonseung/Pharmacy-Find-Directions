c31bcfa67ba76371142441553e985dd5e616878d
lsh <tmdgns4029@naver.com>
2024-08-03 16:00:53 +0900

    [리펙터링] refector : KakaoApiResponse record 사용 시 objectMapper.writeValueAsString(expectedResponse) 호출 때 직렬화가 안됨 따라서 클래스로 변경함

--- src/main/java/com/backend/finddirections/api/dto/KakaoApiResponse.java
+++ src/main/java/com/backend/finddirections/api/dto/KakaoApiResponse.java
@@ -1,11 +1,19 @@
 package com.backend.finddirections.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
-public record KakaoApiResponse(
+// record 사용 시 objectMapper.writeValueAsString(expectedResponse) 직렬화 안됨 그래서 클래스로 변경
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class KakaoApiResponse {
 
-        Meta meta,
-        List<Document> documents
+    private Meta meta;
+    private List<Document> documents;
 
-) {
+
 }
---
+++ src/main/java/com/backend/finddirections/config/RetryConfig.java
@@ -0,0 +1,2 @@
+package com.backend.finddirections.config;public class RetryConfig {
+}
